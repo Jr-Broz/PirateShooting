99d77aac2bd3668f44948df134dd525880dddb71
João Rafael Broz dos Santos <127759756+Jr-Broz@users.noreply.github.com>
2023-08-23 09:00:37 -0300

    Updated Canhao.cs implementing Thread.Sleep

--- Canhao.cs
+++ Canhao.cs
@@ -2,39 +2,81 @@ using System;
 using System.Collections.Generic;
 using PirateGame;
 using System.Text;
+using System.Threading;
+using System.Diagnostics.Contracts;
 
 namespace PirateShooting {
-
     public class Canhao {
 
         string atirar, limpar, recarregar;
+        bool jaFoiAtirado;
+        bool estaLimpo;
+        bool estaRecarregado;
+        bool Executavel;
         Random acertar;
 
-      //Shoot cannon
+        //Shoot cannon
         public string Atirar() {
 
-            atirar = "Atirando canhao";
+            atirar = "ATIRANDO CANHÃO";
             Console.WriteLine(atirar);
-            return this.atirar;
+            estaLimpo = false;
 
-        }
-      
+            if(estaLimpo == false) {
+
+
+                Thread.Sleep(1500);
+                jaFoiAtirado = true;
+         }
+            
+            return this.atirar;            
+        }             
+
         //Clean cannon
         public string Limpar() {
 
-            limpar = "Limpando Canhao !";
-            Console.WriteLine(limpar);
-            return limpar;
-        } 
+            limpar = "CANHÃO LIMPO, PRONTO PARA USO";
+            estaLimpo = false;
+
+            if(estaLimpo == false) {
+
+                Console.WriteLine("ESTAMOS LIMPANDO O CANHAO, CAPITÃO!");
+                Thread.Sleep(2000);
+                Console.WriteLine(limpar);
+                estaLimpo = true;
+                Executavel = true;
+
+
+                if (Executavel == false && estaLimpo == true) {
+
+
+                    Console.WriteLine("Já ESTA LIMPO, NAO É PRECISO LIMPAR DENOVO.");
+                    Executavel = false;
+                }
+            }
+            Console.WriteLine("---------------------------------");
+            return this.limpar;
+            
+        }
 
         //Reload Cannon
         public string Recarregar() {
 
             recarregar = "Recarregando... ";
             Console.WriteLine(recarregar);
+            estaRecarregado = false;
+            Executavel = true;  
+
+            if(estaRecarregado == false) {
+                Console.WriteLine("ESTAMOS RECARREGANDO O CANHAO, CAPITAO!");
+                Thread.Sleep(3500);
+                Console.WriteLine("CANHÃO RECARREGADO E PRONTO PARA UTILIZAR!");
+                estaRecarregado = true;
+                
+            }
+            Console.WriteLine("---------------------------------");
             return recarregar;
         }
-
         //Check if you hit the other "Ship" or not.
         public int Acertar() {
 
@@ -46,9 +88,9 @@ namespace PirateShooting {
             } else {
 
                 Console.WriteLine("Acertamos capitao!");
-
-                return acertar.Next(1, 10);
+           
             }
+            Console.WriteLine("---------------------------------");
             return 0;
         }
     }
