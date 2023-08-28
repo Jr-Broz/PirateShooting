9ac8b8f67b9eca3f44371994c9aaa86b03cc033e
João Rafael Broz dos Santos <127759756+Jr-Broz@users.noreply.github.com>
2023-08-28 11:50:34 -0300

    Removi alguns metodos desnecessarios de pyhsicalCombat.cs

--- pyhsicalCombat.cs
+++ pyhsicalCombat.cs
@@ -3,26 +3,19 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-
 namespace PirateGame {
     interface pyhsicalCombat {
-      
+
         //Modificadores de acesso de interface é public ou abstract. Portanto não faria muita diferença  utilizar uma classe abstrata ou n.
         //Interfaces podem possuir somente métodos e propriedades.
 
-    void EscolherArma();
+        void EscolherArma();
 
-     void AtirarTiro();
+        void AtirarTiro();
 
-    void Golpear();
-
-    void  ChecarSeAcertouGolpeFisico();
-
-     int darDano();
-     int receberDano();
-
-     int acertouTiro();
-
-    
+        int Golpear();
+ 
+        int darDano();
+        int receberDano();
     }
 }
