c387ce9835daf84a80ec443428fa0889efc82eb1
João Rafael Broz dos Santos <127759756+Jr-Broz@users.noreply.github.com>
2023-09-06 09:10:52 -0300

    Removi alguns "using" desnecessários

--- pyhsicalCombat.cs
+++ pyhsicalCombat.cs
@@ -1,7 +1,4 @@
 using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
 using System.Threading.Tasks;
 namespace PirateGame {
     interface pyhsicalCombat {
@@ -14,7 +11,7 @@ namespace PirateGame {
         void AtirarTiro();
 
         void Golpear();
- 
+
         int darDano();
         int receberDano();
     }
